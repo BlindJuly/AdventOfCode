61b323e064cd0f84405f3f64f05e307a0c8c895a
BlindJuly <grantazach@gmail.com>
2022-01-15 23:43:36 -0600

    Update 2021-1 (Increasing Depths Counter).py

--- 2021-1 (Increasing Depths Counter).py
+++ 2021-1 (Increasing Depths Counter).py
@@ -13,9 +13,9 @@ def depth_increase(depth_list):
     return ((len(depth_list) - 1) - count)/2 + count  #since we subtracted 1 when there was a decrease, that means
                                                       #the decreases were "counted twice" in context of finding
                                                       #total increases.  Therefore, You get the final count and
-                                                      #find the difference between it and the total possible increases.
+                                                      #find the difference between it and the total possible increases (len(list) - 1).
                                                       #Since the decreases were counted twice, only half this difference
-                                                      #are actual decreases and therefore the other half are increases.
+                                                      #are actual decreases and therefore the other half are increases.  You add those to your count to find total increases.
 
 
 print(depth_increase(depths))
