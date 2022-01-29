c476f0b72b844ced7836742c0a161727d2a98af0
BlindJuly <grantazach@gmail.com>
2022-01-28 22:20:56 -0600

    Dive! Problem 2 AoC
    Originally my checks were len(move), but I thought just checking the first character in each string would be more simple.

---
+++ Dive_P2_AoC_V2.py
@@ -0,0 +1,18 @@
+
+with open('movements_Dive.txt') as f:
+    moves = f.readlines()
+
+print(moves)
+
+count_v = 0
+count_h = 0
+for move in moves:
+    check_char = move[0]
+    if check_char == 'd':
+        count_v += int(move[5])
+    elif check_char == 'f':
+        count_h += int(move[8])
+    else:
+        count_v -= int(move[3])
+
+print(count_h * count_v)
